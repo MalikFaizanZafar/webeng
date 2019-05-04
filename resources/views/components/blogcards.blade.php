e98580364858568c192fcface73cd94a439c5d49
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-05-04 11:35:04 +0500

    completed welcome page design

---
+++ resources/views/components/blogcards.blade.php
@@ -0,0 +1,50 @@
+<div class="row">
+  <div class="card col-md-4 col-sm-6 col-xs-12 p-0 mb-5">
+    <img class="card-img-top" src="https://images.pexels.com/photos/908284/pexels-photo-908284.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Blog title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card col-md-4 col-sm-6 col-xs-12 p-0 mb-5">
+    <img class="card-img-top" src="https://images.pexels.com/photos/158826/structure-light-led-movement-158826.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Blog title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card col-md-4 col-sm-6 col-xs-12 p-0 mb-5">
+    <img class="card-img-top" src="https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Blog title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card col-md-4 col-sm-6 col-xs-12 p-0 mb-5">
+    <img class="card-img-top" src="https://images.pexels.com/photos/908284/pexels-photo-908284.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Blog title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card col-md-4 col-sm-6 col-xs-12 p-0 mb-5">
+    <img class="card-img-top" src="https://images.pexels.com/photos/158826/structure-light-led-movement-158826.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Blog title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card col-md-4 col-sm-6 col-xs-12 p-0 mb-5">
+    <img class="card-img-top" src="https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Blog title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
