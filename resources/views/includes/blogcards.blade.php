180196532a28c370b687a9ee5138a496d95cee7f
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-05-06 06:27:57 +0500

    connected to backend and getting data from backend

--- resources/views/includes/blogcards.blade.php
+++ resources/views/includes/blogcards.blade.php
@@ -1,68 +1,15 @@
 <div class="row">
-  <div class="card col-md-3 mx-5 col-sm-6 col-xs-12 p-0 mb-5">
-    <img class="card-img-top" src="https://images.pexels.com/photos/908284/pexels-photo-908284.jpeg" alt="Card image cap">
+@foreach ($blogs as $blog)
+<div class="card col-md-3 mx-5 col-sm-6 col-xs-12 p-0 mb-5">
+    <img class="card-img-top" src="{{$blog->image_url}}" alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Blog title</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <h5 class="card-title">{{$blog->title}}</h5>
+      <p class="card-text">{{substr($blog->description, 0, 100)}}</p>
     
     </div>
     <div class=" mr-2 mb-2 text-muted d-flex justify-content-end">
     <a href="{{url('blog')}}" class="btn btn-primary">View Blog</a>
   </div>
   </div>
-  <div class="card col-md-3 mx-5 col-sm-6 col-xs-12 p-0 mb-5">
-    <img class="card-img-top" src="https://images.pexels.com/photos/158826/structure-light-led-movement-158826.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Blog title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    
-    </div>
-    <div class=" mr-2 mb-2 text-muted d-flex justify-content-end">
-    <a href="{{url('blog')}}" class="btn btn-primary">View Blog</a>
-  </div>
-  </div>
-  <div class="card col-md-3 mx-5 col-sm-6 col-xs-12 p-0 mb-5">
-    <img class="card-img-top" src="https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Blog title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    
-    </div>
-    <div class=" mr-2 mb-2 text-muted d-flex justify-content-end">
-    <a href="{{url('blog')}}" class="btn btn-primary">View Blog</a>
-  </div>
-  </div>
-  <div class="card col-md-3 mx-5 col-sm-6 col-xs-12 p-0 mb-5">
-    <img class="card-img-top" src="https://images.pexels.com/photos/908284/pexels-photo-908284.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Blog title</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    
-    </div>
-    <div class=" mr-2 mb-2 text-muted d-flex justify-content-end">
-    <a href="{{url('blog')}}" class="btn btn-primary">View Blog</a>
-  </div>
-  </div>
-  <div class="card col-md-3 mx-5 col-sm-6 col-xs-12 p-0 mb-5">
-    <img class="card-img-top" src="https://images.pexels.com/photos/158826/structure-light-led-movement-158826.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Blog title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    
-    </div>
-    <div class=" mr-2 mb-2 text-muted d-flex justify-content-end">
-    <a href="{{url('blog')}}" class="btn btn-primary">View Blog</a>
-  </div>
-  </div>
-  <div class="card col-md-3 mx-5 col-sm-6 col-xs-12 p-0 mb-5">
-    <img class="card-img-top" src="https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Blog title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    
-    </div>
-    <div class=" mr-2 mb-2 text-muted d-flex justify-content-end">
-    <a href="{{url('blog')}}" class="btn btn-primary">View Blog</a>
-  </div>
-  </div>
+@endforeach
 </div>
--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    $blogs = DB::select("select * from blogs");
+    return view('welcome',['blogs' => $blogs]);
 });
 
 Route::get('/signup', function () {
@@ -33,4 +33,14 @@ Route::get('/blog', function () {
 
 Route::get('/about', function () {
     return view('about');
+});
+
+Route::get('/blog/create', function () {
+    $image_url = 'https://images.pexels.com/photos/373543/pexels-photo-373543.jpeg';
+    $description = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum';
+    $title = 'Blog Title';
+    for ($x = 0; $x <= 5; $x++) {
+        DB::insert('insert into blogs (title, image_url, description, author_id, date_created) values (:title, :image_url, :description, :author_id, :date_created)', ['title' => 'blog title', 'image_url' => $image_url, 'description' => $description, 'author_id' => 3, 'date_created' => '2019-05-10 23:59:59']);
+    }
+    return "Five Blogs Created";
 });
